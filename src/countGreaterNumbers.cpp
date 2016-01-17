dc2dd63eb3f5390250b9b904ab11b2432f61ef07
meshubh <meshubh@missionrnd.com>
2016-01-17 15:47:33 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,53 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
-struct transaction {
+#include<stdio.h>
+struct transaction
+{
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+int countGreaterNumbers(struct transaction *Arr, int len, char *date)
+{
+	int given_day, given_month, given_year, arr_day, arr_month, arr_year;
+	given_day = date[0] * 10 + date[1];
+	given_month = date[3] * 10 + date[4];
+	given_year = date[6] * 1000 + date[7] * 100 + date[8] * 10 + date[9];
+	int iterator = 0, count = 0;
+	while (iterator < len)
+	{
+		arr_year = Arr[iterator].date[6] * 1000 + Arr[iterator].date[7] * 100 + Arr[iterator].date[8] * 10 + Arr[iterator].date[9];
+		if (arr_year > given_year)
+		{
+			count++;
+			iterator++;
+		}
+		else if (arr_year == given_year)
+		{
+			arr_month = Arr[iterator].date[3] * 10 + Arr[iterator].date[4];
+			if (arr_month > given_month)
+			{
+				count++;
+				iterator++;
+			}
+			else if (arr_month == given_month)
+			{
+				arr_day = Arr[iterator].date[0] * 10 + Arr[iterator].date[1];
+				if (arr_day > given_day)
+				{
+					count++;
+					iterator++;
+				}
+				else
+					iterator++;
+			}
+			else
+				iterator++;
+		}
+		else
+			iterator++;
+	}
+	return count;
 }
