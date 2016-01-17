cbbb6a1df6f82928b47be539dd8144e73e155aa4
meshubh <meshubh@missionrnd.com>
2016-01-17 15:53:19 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -12,7 +12,8 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include<stdio.h>
+#include<malloc.h>
 #include <iostream>
 
 struct transaction {
@@ -21,6 +22,154 @@ struct transaction {
 	char description[20];
 };
 
-struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen)
+{
+	if (A == NULL || B == NULL)
+		return NULL;
+	else
+	{
+		struct transaction *output = (struct transaction *)malloc((ALen + BLen)*sizeof(struct transaction));
+		int iterator1 = 0, iterator2 = 0, iterator3 = 0, fin_len, arr1_year, arr1_month, arr1_day, arr2_day, arr2_month, arr2_year;
+		fin_len = BLen + ALen;
+		while (iterator1 + iterator2 < fin_len)
+		{
+			arr1_year = A[iterator1].date[6] * 1000 + A[iterator1].date[7] * 100 + A[iterator1].date[8] * 10 + A[iterator1].date[9];
+			arr2_year = B[iterator2].date[6] * 1000 + B[iterator2].date[7] * 100 + B[iterator2].date[8] * 10 + B[iterator2].date[9];
+			if (arr1_year > arr2_year)
+			{
+				if (iterator2 < BLen)
+				{
+					output[iterator3] = B[iterator2];
+					iterator3++;
+					iterator2++;
+				}
+				else
+				{
+					while (iterator1 < ALen)
+					{
+						output[iterator3] = A[iterator1];
+						iterator3++;
+						iterator1++;
+					}
+					break;
+				}
+			}
+			else if (arr1_year<arr2_year)
+			{
+				if (iterator1 < ALen)
+				{
+					output[iterator3] = A[iterator1];
+					iterator3++;
+					iterator1++;
+				}
+				else
+				{
+					while (iterator2 < BLen)
+					{
+						output[iterator3] = B[iterator2];
+						iterator2++;
+						iterator3++;
+					}
+					break;
+				}
+
+			}
+			else
+			{
+				arr1_month = A[iterator1].date[3] * 10 + A[iterator1].date[4];
+				arr2_month = B[iterator2].date[3] * 10 + B[iterator2].date[4];
+				if (arr1_month > arr2_month)
+				{
+					if (iterator2 < BLen)
+					{
+						output[iterator3] = B[iterator2];
+						iterator3++;
+						iterator2++;
+					}
+					else
+					{
+						while (iterator1 < ALen)
+						{
+							output[iterator3] = A[iterator1];
+							iterator3++;
+							iterator1++;
+						}
+						break;
+					}
+				}
+				else if (arr1_month<arr2_month)
+				{
+					if (iterator1 < ALen)
+					{
+						output[iterator3] = A[iterator1];
+						iterator3++;
+						iterator1++;
+					}
+					else
+					{
+						while (iterator2 < BLen)
+						{
+							output[iterator3] = B[iterator2];
+							iterator2++;
+							iterator3++;
+						}
+						break;
+					}
+				}
+				else
+				{
+					arr1_day = A[iterator1].date[0] * 10 + A[iterator1].date[1];
+					arr2_day = B[iterator2].date[0] * 10 + B[iterator2].date[1];
+					if (arr1_day > arr2_day)
+					{
+						if (iterator2 < BLen)
+						{
+							output[iterator3] = B[iterator2];
+							iterator3++;
+							iterator2++;
+						}
+						else
+						{
+							while (iterator1 < ALen)
+							{
+								output[iterator3] = A[iterator1];
+								iterator3++;
+								iterator1++;
+							}
+							break;
+						}
+					}
+					else if (arr1_day<arr2_day)
+					{
+						if (iterator1 < ALen)
+						{
+							output[iterator3] = A[iterator1];
+							iterator3++;
+							iterator1++;
+						}
+						else
+						{
+							while (iterator2 < BLen)
+							{
+								output[iterator3] = B[iterator2];
+								iterator2++;
+								iterator3++;
+							}
+							break;
+						}
+					}
+					else
+					{
+						output[iterator3] = A[iterator1];
+						iterator3++;
+						iterator1++;
+						output[iterator3] = B[iterator2];
+						iterator3++;
+						iterator2++;
+					}
+				}
+			}
+		}
+		return output;
+	}
 }
